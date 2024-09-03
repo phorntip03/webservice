97785a5b65375431e1f51e06fd1dd1dee4a6068a
phorntip03 <oopontip7524@gmail.com>
2024-09-03 15:02:05 +0700

    up

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
-    //
+    public function login(Request $req){
+        dd($req->email."".$req->password);
+        Validator::make($req -> all(),[
+
+        ]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@
 use App\Http\Controllers\Api\TeacherController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 
 
@@ -11,8 +12,9 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::resource('teacher',TeacherController::class);
-Route::post('login',[AuthController::class.'login']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('login',[AuthController::class,'login']);
+
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+   // return $request->user();
+//});
